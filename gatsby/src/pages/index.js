01743d0a0bc91159c190b872fab1a81750c8ed5f
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-01-08 17:29:54 +0800

    change

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -23,7 +23,7 @@ function HotSlices({ hotSlices }) {
   return (
     <div>
       <h2 className="center">
-        <span className="mark tilt">Hot Slices</span>
+        <span className="mark tilt">Hot Slices!</span>
       </h2>
       <p>Come on by, buy the slice!</p>
       {!hotSlices && <LoadingGrid count={4} />}
